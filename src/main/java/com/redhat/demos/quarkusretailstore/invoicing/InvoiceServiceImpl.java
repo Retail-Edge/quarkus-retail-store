8b83f28ba22704e43a5cf01828b1a7807c895cfc
jeremyrdavis <jeremy.davis@redhat.com>
2021-09-10 10:45:35 -0400

    saving outbox event

--- src/main/java/com/redhat/demos/quarkusretailstore/invoicing/InvoiceServiceImpl.java
+++ src/main/java/com/redhat/demos/quarkusretailstore/invoicing/InvoiceServiceImpl.java
@@ -8,10 +8,12 @@ import com.redhat.demos.quarkusretailstore.invoicing.domain.Invoice;
 import com.redhat.demos.quarkusretailstore.invoicing.domain.InvoiceEventResult;
 import com.redhat.demos.quarkusretailstore.invoicing.domain.InvoiceRepository;
 import com.redhat.demos.quarkusretailstore.products.ProductMaster;
+import io.debezium.outbox.quarkus.ExportedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
@@ -25,6 +27,9 @@ public class InvoiceServiceImpl implements InvoiceService {
     @Inject
     InvoiceRepository invoiceRepository;
 
+    @Inject
+    Event<ExportedEvent<?, ?>> event;
+
     @Override @Transactional
     public InvoiceDTO createInvoice(final InvoiceDTO invoiceDTO) {
 
@@ -33,6 +38,9 @@ public class InvoiceServiceImpl implements InvoiceService {
         LOGGER.debug("invoice created: {}", invoiceEventResult.getInvoice());
         Invoice invoice = invoiceRepository.persist(invoiceEventResult.getInvoice());
         LOGGER.debug("invoice persisted {}", invoice);
+        invoiceEventResult.getInvoiceEvents().forEach(invoiceEvent -> {
+            event.fire(invoiceEvent);
+        });
         return new InvoiceDTO(
                 invoice.getId(),
                 new InvoiceHeaderDTO(invoice.getInvoiceHeader()),
